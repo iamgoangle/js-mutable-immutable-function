135d6d2c75e797a46f1e8b065a61662631768ef2
Teerapong Singthong <st.teerapong@gmail.com>
2016-09-19 12:05:10 +0700

    Add files via upload

---
+++ mutator/copyWithin.js
@@ -0,0 +1,14 @@
+/*
+    target	Required. The index position to copy the elements to
+    start	Required. The index position to start copying elements from
+    end	Optional. The index position to stop copying elements from (default is array.length)
+*/
+
+const arraySet = ["Apple", "Banana", "Coconut", "Donut"]
+
+console.log(arraySet.copyWithin(2, 0, 1))   // Apple, Banana, Apple, Donut
+
+// trying to console source of array
+
+// the result is that [Apple, Banana, Apple, Donut] since this it was mutate value prior event
+console.log(arraySet)
